38a224ff9b6ece3c641ea2f40641d9795c0be285
sjlevin <sjlevin@gmail.com>
2020-08-18 01:21:12 +0000

    Deploying to gh-pages from  @ 5ddeb658b60187f3e4ae13861afe9d411ccd3af2 🚀

---
+++ precache-manifest.54453add42b2a0d896f185d68a0b09f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cb5b0ed237c7f47cf4dc4ca5363d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28bec4035eb319d23347",
+    "url": "/static/js/2.08369e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08369e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b19bd69f02be0e8f218",
+    "url": "/static/js/main.bea7b164.chunk.js"
+  },
+  {
+    "revision": "5ab360f9185cb2436805",
+    "url": "/static/js/runtime-main.7a4f0968.js"
+  }
+]);
